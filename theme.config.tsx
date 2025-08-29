e8edc375f681af5f71726a4902a017761819af8d
Li <wli@chapman.edu>
2025-08-29 14:31:16 -0700

    Nextra site

---
+++ theme.config.tsx
@@ -0,0 +1,6 @@
+export default {
+  logo: 'ThinkExpo',
+  project: { link: 'https://github.com/JustinWenzhaoLi/thinkexpo' },
+  docsRepositoryBase: 'https://github.com/JustinWenzhaoLi/thinkexpo/blob/main',
+  footer: { text: `© ${new Date().getFullYear()} Wenzhao (Justin) Li` }
+}
